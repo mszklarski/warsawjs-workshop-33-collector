86f1b550a80bd99fa0191a009deddcc8370a21a5
Michał Szklarski <mszklarski@e-point.pl>
2019-06-23 13:51:06 +0200

    Bootstrap

---
+++ collector/src/components/Layout.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app id="inspire">
+    <v-toolbar color="indigo" dark fixed app>
+      <v-toolbar-title>Collector : your debt listing</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout
+          justify-center
+          align-center
+        >
+          <v-flex text-xs-center>
+            
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer color="indigo" app>
+      <span class="white--text">Collector &copy; {{ currentYear }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: null
+    }),
+    props: {
+      source: String
+    },
+    computed: {
+      currentYear: function () {
+        return new Date().getFullYear()
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
